18d8b6639c270f54f2c12ca189093b3281233387
Elon Young <donghao1@student.unimelb.edu.au>
2024-11-01 09:41:50 +1100

    Create the database table

--- app/src/main/java/com/example/soundsenseapp/MainActivity.java
+++ app/src/main/java/com/example/soundsenseapp/MainActivity.java
@@ -1,20 +1,29 @@
 package com.example.soundsenseapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import com.example.soundsenseapp.Spotify.DatabaseHelper;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import org.json.JSONException;
-
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dbHelper = new DatabaseHelper(this);
+        try {
+            dbHelper.getWritableDatabase();
+            Log.d("MainActivity", "Database connected successfully and tables created if not exist.");
+        } catch (Exception e) {
+            Log.e("MainActivity", "Database connection failed: " + e.getMessage());
+        }
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
